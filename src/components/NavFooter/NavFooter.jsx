4960c397a9e6f0eb9bd86e0bc56075b5b6a4b024
yuelinwen <tyleryuelinwen@gmail.com>
2022-02-13 02:05:06 -0500

    add media query, responsive app

--- src/components/NavFooter/NavFooter.jsx
+++ src/components/NavFooter/NavFooter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 import { ReactComponent as RiotGamesLogo } from "../../assets/riot-games-logo.svg";
 import { HomeOutlined, DesktopOutlined, UserOutlined } from "@ant-design/icons";
@@ -9,22 +9,23 @@ import "./NavFooter.css";
 const NavFooter = () => {
   return (
     <Navbar bg="light" variant="light" fixed="bottom">
-      <Container>
-        <Navbar.Brand>
-          <RiotGamesLogo style={{fontSize:4}}/>
-        </Navbar.Brand>
-        <Nav className="me-auto nav-options">
-          <Nav.Link href="#">
-            <HomeOutlined style={{ fontSize: "400%" }} />
-          </Nav.Link>
-          <Nav.Link href="#">
-            <DesktopOutlined style={{ fontSize: "400%" }} />
-          </Nav.Link>
-          <Nav.Link href="#">
-            <UserOutlined style={{ fontSize: "400%" }} />
-          </Nav.Link>
-        </Nav>
-      </Container>
+      <Navbar.Brand className="nav-logo">
+        <RiotGamesLogo className="logo" />
+      </Navbar.Brand>
+      <Nav className="me-auto nav-options">
+        <Nav.Link className="nav-item" href="#">
+          <HomeOutlined className="nav-icon" />
+          <p className="nav-p">Home</p>
+        </Nav.Link>
+        <Nav.Link className="nav-item" href="#">
+          <DesktopOutlined className="nav-icon" />
+          <p className="nav-p">Desk</p>
+        </Nav.Link>
+        <Nav.Link className="nav-item" href="#">
+          <UserOutlined className="nav-icon" />
+          <p className="nav-p">Profile</p>
+        </Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
