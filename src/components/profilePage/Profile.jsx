02f60f61adfadb93f81f337293bd151be47bd0a2
longswordfish <robinyu9840@gmail.com>
2022-02-14 14:30:30 -0500

    try style from Figma

--- src/components/profilePage/Profile.jsx
+++ src/components/profilePage/Profile.jsx
@@ -1,16 +1,11 @@
-import { Button } from "antd";
 import "./Profile.css";
 
 const ProfilePage = ()=>(
     <div className="profile">
       <h1>Hello Welcome</h1>
-      <div className="buttons">
-        <Button type="default" size="large" block>
-          LOG OUT
-        </Button>
-        <Button type="default" size="large" block>
-          SETTINGS
-        </Button>
+      <div className="profile-links">
+        <div className="link-to-other-feature">LOG OUT</div>
+        <div className="link-to-other-feature">SETTINGS</div>
       </div>
     </div>
 )
