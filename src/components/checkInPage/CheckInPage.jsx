650a3983736b6220c041ebcf4473351af3a903ae
longswordfish <robinyu9840@gmail.com>
2022-03-11 18:28:51 -0500

    check in page done

--- src/components/checkInPage/CheckInPage.jsx
+++ src/components/checkInPage/CheckInPage.jsx
@@ -4,6 +4,9 @@ import CovidSurvey from "../covidSurvey/CovidSurvey.component";
 
 import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
+import { Navigate, useNavigate } from "react-router-dom";
+
+import { Button } from "antd";
 
 import "./CheckInPage.styles.css";
 
@@ -12,6 +15,10 @@ function CheckInPage() {
 
   const [hasBooking, setHasBooking] = useState(false);
   const [checkedIn, setCheckedIn] = useState(false);
+  const [answer,setAnswer] = useState([]);
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     const date = "2022-04-03";
     if (fakeDB.userHasBookingThatDay("Tom", date)) {
@@ -22,13 +29,33 @@ function CheckInPage() {
     }
   }, [currentUser]);
 
+  const changeAnswer = values=>setAnswer(values);
+
+  const handleSubmit = ()=>{
+    console.log(`answer is ${answer}`);
+    console.log(`currentUser is ${currentUser}`);
+    alert("Check in successful!");
+    navigate("/");
+  }
+
+  const handleNavigate=()=>{
+    navigate("/");
+  }
+
   return (
     <div className="checkin-page">
       <h2>Check In</h2>
       <div className="checkin-container">
         {hasBooking ? (
           checkedIn === false ? (
-            <CovidSurvey />
+            <div >
+              <CovidSurvey changeAnswer={changeAnswer}/>
+              <div className="buttons-wrapper-in-checkin" style={{"marginLeft":"38%","marginTop":"20px"}}>
+                <Button  type="primary" onClick={handleSubmit}>Check In</Button>
+                <Button type="danger" onClick={handleNavigate} style={{"marginLeft":"20px"}}>Back to Home</Button>
+              </div>
+               </div>
+            
           ) : (
             <div className="already-checkin">You already checked in today</div>
           )
--- src/components/covidSurvey/CovidSurvey.component.jsx
+++ src/components/covidSurvey/CovidSurvey.component.jsx
@@ -1,21 +1,34 @@
-import { Form } from "antd";
+import { Checkbox } from "antd";
 import "./CovidSurvey.styles.css";
 
-function CovidSurvey() {
+function CovidSurvey({changeAnswer}) {
+
+  function onChange(checkedValues) {
+    changeAnswer(checkedValues);
+  }
+  
   const questions = [
-    "Are you fully vaccinated against COVID-19?",
-    "Are you feeling severe difficulty breathing?",
-    "Are you feeling severe check pain?",
-    "In the last 14 days, have you travelled outside of Canada?",
-    "In the last 5 days, have you experienced fever and/or chills?",
-    "In the last 5 days, have you experienced cough or barking cough?",
+    { label: "Are you fully vaccinated against COVID-19?", value: 1 },
+    { label: "Are you feeling severe difficulty breathing?", value: 2 },
+    { label: "Are you feeling severe check pain?", value: 3 },
+    { label: "In the last 14 days, have you travelled outside of Canada?", value: 4},
+    { label: "In the last 5 days, have you experienced fever and/or chills?", value: 5 },
+    { label: "In the last 5 days, have you experienced cough or barking cough?", value: 6 }
   ];
+
 
   return (
     <div className="covid-survey">
-      <h3>Covid Survey</h3>
+      {/* <h3>Covid Survey</h3> */}
       <div className="covid-survey-container">
-        <Form></Form>
+        <div className="covid-survey-title">
+          <span style={{"width":"10%"}}>Choose</span>
+          <span style={{"textAlign":"center","width":"90%"}}>Questions</span>
+        </div>
+          <Checkbox.Group
+            options={questions}
+            onChange={onChange}
+          />
       </div>
     </div>
   );
